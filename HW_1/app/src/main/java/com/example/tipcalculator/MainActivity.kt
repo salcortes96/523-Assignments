ad0ac35604a37988fa25574336458b16d1d0281c
salcortes96 <salcortes96@gmail.com>
2021-07-14 23:28:30 -0700

    7/14/2021

--- HW_1/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ HW_1/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AbsSeekBar
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        seekBarTip.setOnSeekBarChangeListener(object: seekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged( seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i(TAG, msg: "onProgresChanged $progress")
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        } )
     }
 }
+
